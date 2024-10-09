872edf0af91860825b4b602a8cd5bca06d8db049
ZloiAdmiN <96934475+Whats-bot@users.noreply.github.com>
2024-10-09 13:13:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import moment from 'moment-timezone'
 
 //---------[ Añada los numeros a ser Propietario/a ]---------
 
-global.owner = [['79885715007', 'ＰＲＯＰＩＥＴＡＲＩＯ', true], ['79081840670'], ['79612857835'], ['79185451060'], ['799282292265']]
+global.owner = [['79885715007', 'ＰＲＯＰＩＥＴＡＲＩＯ', true], ['79081840670'], ['79612857835'], ['79185451060'], ['79282292265']]
 global.mods = []
 global.prems = [79885715007]
 
